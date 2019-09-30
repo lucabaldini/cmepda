09acc3d7b14ddeff986cab5332819e3f7ab6e4f6
Luca Baldini <luca.baldini@pi.infn.it>
2019-09-30 08:26:01 +0200

    Initial import.

---
+++ slides/latex/snippets/import1.py
@@ -0,0 +1,7 @@
+# This is ok, and vastly recognized by the community
+import numpy as np
+from matplotlib import pyplot as plt
+
+x = np.linspace(0., 10., 100)
+y = x**2.
+plt.plot(x, y)
---
+++ slides/latex/snippets/import2.py
@@ -0,0 +1,6 @@
+from math import *
+import logging as log
+
+# ... 1000 lines of code in the middle
+
+x = log(2.)
