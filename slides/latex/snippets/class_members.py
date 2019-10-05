e62bcb5135135abb5982985b687ee31032d43628
alberto_manfreda <albertom@pi.infn.it>
2019-10-05 18:08:40 +0200

    A bunch of fixes to lesson 5

--- slides/latex/snippets/class_members.py
+++ slides/latex/snippets/class_members.py
@@ -1,8 +1,21 @@
 class Television:
     """ Class describing a televsion.
     """
-    model = "Sv32X-553T" # This is a class variable
+    def add_a_class_member(self):
+        """ Add a class member - this is in the instance namespace (remember
+        the meaning of 'self') """
+        self.current_channel = 1
 
-print(Television.model) # We don't need an instance to access it
 tv = Television()
-print(tv.model) # We can access it also through instances
+tv.add_a_class_member()
+
+# Class members are accessed through the '.' (dot) operator
+print (tv.current_channel)
+
+# Since this is an instance attribute, changing it will not affect other
+# instances of the class
+another_tv = Television()
+another_tv.add_a_class_member()
+tv.current_channel = 5
+# The following line will print 1, not 5
+print(another_tv.current_channel)
--- slides/latex/snippets/everything_is_a_class.py
+++ slides/latex/snippets/everything_is_a_class.py
@@ -4,19 +4,15 @@ this_is_an_int = 5
 print(type(this_is_an_int))
 
 # Same with a string
-this_is_a_string = 'Hello world' 
+this_is_a_string = 'Hello world!' 
 print(type(this_is_a_string))
 
 # Same with a list
-this_is_a_list = [1, 2, 3, 'stella']
+this_is_a_list = ['Frodo', 'Samvise', 'Meriadoc', 'Peregrino']
 print(type(this_is_a_list))
 
-# Now a tuple
-this_is_a_tuple = ('Frodo', 'Samvise', 'Meriadoc', 'Peregrino')
-print(type(this_is_a_tuple))
-
-# And finally a dict
-this_is_a_dict = {'farina'     : '120g',
-                  'latte'      : '170ml',
-                  'uova'       : '1'}
-print(type(this_is_a_dict))
+# And even a function!
+def this_is_a_function():
+    return 0
+    
+print(type(this_is_a_function))
