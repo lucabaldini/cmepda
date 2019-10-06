f8127ad0f4ae970f5cff01657c6d49e3227e920d
alberto_manfreda <albertom@pi.infn.it>
2019-10-06 11:57:11 +0200

    Reorganized the discussion on member variables

--- slides/latex/snippets/class_members.py
+++ slides/latex/snippets/class_members.py
@@ -2,8 +2,7 @@ class Television:
     """ Class describing a televsion.
     """
     def add_a_class_member(self):
-        """ Add a class member - this is in the instance namespace (remember
-        the meaning of 'self') """
+        """ Add a class member (remember the meaning of 'self') """
         self.current_channel = 1
 
 tv = Television()
@@ -12,10 +11,10 @@ tv.add_a_class_member()
 # Class members are accessed through the '.' (dot) operator
 print (tv.current_channel)
 
-# Since this is an instance attribute, changing it will not affect other
-# instances of the class
+# Each instance gets its own copy of the members
 another_tv = Television()
 another_tv.add_a_class_member()
+# Changing the member for one will not affect other instances of the class
 tv.current_channel = 5
 # The following line will print 1, not 5
 print(another_tv.current_channel)
--- slides/latex/snippets/class_methods.py
+++ slides/latex/snippets/class_methods.py
@@ -1,15 +1,16 @@
 class Television:
     """ Class describing a televsion.
     """
-    def turn_on(self): # Class method
+    def turn_on(self, channel=1): # Class method
         """All the class methods get the object istance as their first argument.
         It is customary to call this argument 'self', though is not required
         by the language rules (you can call it 'pippo' and it will work 
         just as well)
         """
-        print('Turning on!')
+        print('Showing channel {}'.format(channel))
 
 tv = Television()
 # Class methods and members are accessed through the '.' (dot) operator
-tv.turn_on()
+# You don't have to pass manually the self argument, it is added for you!
+tv.turn_on(channel=3)
 
