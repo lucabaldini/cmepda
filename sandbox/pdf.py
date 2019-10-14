e5fe61385f10e122be5b33a3f17f217527a75785
Luca Baldini <luca.baldini@pi.infn.it>
2019-10-14 11:09:53 +0200

    Initial import.

---
+++ sandbox/pdf.py
@@ -0,0 +1,62 @@
+
+import numpy as np
+from scipy.interpolate import InterpolatedUnivariateSpline
+from matplotlib import pyplot as plt
+
+
+
+class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
+
+    """Class describing a probability density function.
+    """
+
+    def __init__(self, x, y):
+        """Constructor.
+        """
+        InterpolatedUnivariateSpline.__init__(self, x, y)
+        ycdf = [self.integral(0, xcdf) for xcdf in x]
+        self.cdf = InterpolatedUnivariateSpline(x, ycdf)
+        self.ppf = InterpolatedUnivariateSpline(ycdf, x)
+
+    def prob(self, x1, x2):
+        """Return the probability for the random variable to be included 
+        between x1 and x2.
+        """
+        return self.cdf(x2) - self.cdf(x1)
+
+    def rnd(self, size=1000):
+        """Return an array of random values from the pdf.
+        """
+        return self.ppf(np.random.uniform(size=size))
+    
+
+
+
+if __name__ == '__main__':
+    x = np.linspace(0., 1., 101)
+    y = 2. * x
+    pdf = ProbabilityDensityFunction(x, y)
+    a = np.array([0.2, 0.6])
+    print(pdf(a))
+
+    plt.figure('pdf')
+    plt.plot(x, pdf(x))
+    plt.xlabel('x')
+    plt.ylabel('pdf(x)')
+
+    plt.figure('cdf')
+    plt.plot(x, pdf.cdf(x))
+    plt.xlabel('x')
+    plt.ylabel('cdf(x)')
+
+    plt.figure('ppf')
+    plt.plot(x, pdf.ppf(x))
+    plt.xlabel('x')
+    plt.ylabel('ppf(x)')
+
+    plt.figure('Sampling')
+    rnd = pdf.rnd(1000000)
+    plt.hist(rnd, bins=200)
+    
+
+    plt.show()
