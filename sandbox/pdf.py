91fab9caeefa7f2b75ec510ed3d08d3e68894032
Luca Baldini <luca.baldini@pi.infn.it>
2019-10-17 10:50:48 +0200

    Gaussian test implemented.

--- sandbox/pdf.py
+++ sandbox/pdf.py
@@ -2,6 +2,7 @@
 import numpy as np
 from scipy.interpolate import InterpolatedUnivariateSpline
 from matplotlib import pyplot as plt
+from scipy.optimize import curve_fit
 
 
 
@@ -14,9 +15,14 @@ class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
         """Constructor.
         """
         InterpolatedUnivariateSpline.__init__(self, x, y)
-        ycdf = [self.integral(0, xcdf) for xcdf in x]
+        ycdf = np.array([self.integral(x[0], xcdf) for xcdf in x])
         self.cdf = InterpolatedUnivariateSpline(x, ycdf)
-        self.ppf = InterpolatedUnivariateSpline(ycdf, x)
+        # Need to make sure that the vector I am passing to the ppf spline as
+        # the x values has no duplicates---and need to filter the y
+        # accordingly.
+        xppf, ippf = np.unique(ycdf, return_index=True)
+        yppf = x[ippf]
+        self.ppf = InterpolatedUnivariateSpline(xppf, yppf)
 
     def prob(self, x1, x2):
         """Return the probability for the random variable to be included
@@ -31,8 +37,9 @@ class ProbabilityDensityFunction(InterpolatedUnivariateSpline):
 
 
 
-
-if __name__ == '__main__':
+def test_triangular():
+    """Unit test with a triangular distribution.
+    """
     x = np.linspace(0., 1., 101)
     y = 2. * x
     pdf = ProbabilityDensityFunction(x, y)
@@ -60,4 +67,53 @@ if __name__ == '__main__':
     plt.hist(rnd, bins=200)
 
 
+def test_gauss(mu=0., sigma=1., support=10., num_points=500):
+    """Unit test with a gaussian distribution.
+    """
+    from scipy.stats import norm
+    x = np.linspace(-support * sigma + mu, support * sigma + mu, num_points)
+    y = norm.pdf(x, mu, sigma)
+    pdf = ProbabilityDensityFunction(x, y)
+
+    plt.figure('pdf')
+    plt.plot(x, pdf(x))
+    plt.xlabel('x')
+    plt.ylabel('pdf(x)')
+
+    plt.figure('cdf')
+    plt.plot(x, pdf.cdf(x))
+    plt.xlabel('x')
+    plt.ylabel('cdf(x)')
+
+    plt.figure('ppf')
+    q = np.linspace(0., 1., 1000)
+    plt.plot(q, pdf.ppf(q))
+    plt.xlabel('q')
+    plt.ylabel('ppf(q)')
+
+    plt.figure('Sampling')
+    rnd = pdf.rnd(1000000)
+    ydata, edges, _ = plt.hist(rnd, bins=200)
+    xdata = 0.5 * (edges[1:] + edges[:-1])
+
+    def f(x, C, mu, sigma):
+        return C * norm.pdf(x, mu, sigma)
+    
+    popt, pcov = curve_fit(f, xdata, ydata)
+    print(popt)
+    print(np.sqrt(pcov.diagonal()))
+    _x = np.linspace(-10, 10, 500)
+    _y = f(_x, *popt)
+    plt.plot(_x, _y)
+
+    mask = ydata > 0
+    chi2 = sum(((ydata[mask] - f(xdata[mask], *popt)) / np.sqrt(ydata[mask]))**2.)
+    nu = mask.sum() - 3
+    sigma = np.sqrt(2 * nu)
+    print(chi2, nu, sigma)
+    
+
+
+if __name__ == '__main__':
+    test_gauss()
     plt.show()
