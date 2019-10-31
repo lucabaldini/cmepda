85ee8b316a83cd914620ace30ce33b4fd9a40ffc
alberto_manfreda <albertom@pi.infn.it>
2019-10-31 09:11:31 +0100

    Small revert

--- slides/latex/snippets/julia/julia_playground.py
+++ slides/latex/snippets/julia/julia_playground.py
@@ -27,13 +27,12 @@ def show_greyscale(output_raw, width, height, max_iterations):
 
 def calculate_z_serial_purepython(maxiter, zs, cs):
     """Calculate output list using Julia update rule"""
-    local_abs = abs
     output = [0] * len(zs)
     for i in range(len(zs)):
         n = 0
         z = zs[i]
         c = cs[i]
-        while local_abs(z) < 2 and n < maxiter:
+        while abs(z) < 2 and n < maxiter:
             z = z * z + c
             n += 1
         output[i] = n
