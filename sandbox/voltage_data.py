3a956557a8296cc94e020ee4065f56afecf93f1f
albertom <albertom@pi.infn.it>
2020-10-12 18:07:40 +0200

    Voltage data solution

--- sandbox/voltage_data.py
+++ sandbox/voltage_data.py
@@ -1,14 +1,67 @@
 import numpy
 from matplotlib import pyplot as plt
+from scipy import interpolate
 
 
 class VoltageData:
-   """Class for handling a set of measurements of the voltage at different
-   times."""
-   
-   pass
-   
-   
+    """Class for handling a set of measurements of the voltage at different
+    times."""
+
+    def __init__(self, times, voltages):
+        """ times and voltages are two iterables of the same length."""
+        t = numpy.array(times, dtype=numpy.float64)
+        v = numpy.array(voltages, dtype=numpy.float64)
+        self._data = numpy.column_stack([t, v])
+        self.spline = interpolate.InterpolatedUnivariateSpline(self.timestamps,
+                                                               self.voltages,
+                                                               k=3)
+
+    @property
+    def timestamps(self):
+        return self._data[:, 0]
+
+    @property
+    def voltages(self):
+        return self._data[:, 1]
+
+    def __len__(self):
+        """ Return the number of entries (measurements)."""
+        return len(self._data)
+
+    def __getitem__(self, index):
+        """Random access"""
+        return self._data[index]
+
+    def __iter__(self):
+        return iter(self._data)
+
+    def __str__(self):
+        """ Print values row-by-row."""
+        row_fmt = '{:d}) {:.1f} {:.2f}'
+        output_rows = []
+        for i, entry in enumerate(self):
+            row = row_fmt.format(i, entry[0], entry[1])
+            output_rows.append(row)
+        return '\n'.join(output_rows)
+        #return '\n'.join([row_fmt.format(i, entry[0], entry[1]) \
+        #                  for i, entry in enumerate(self)])
+
+    def plot(self, ax=None, fmt='bo--', **kwargs):
+        """ Plot the data using matplotlib.pyplot."""
+        if ax is not None:
+            plt.sca(ax)
+        else:
+            plt.figure('voltages vs times')
+        plt.plot(self.timestamps, self.voltages, fmt, **kwargs)
+        plt.xlabel('Time [s]')
+        plt.ylabel('Voltage [mV]')
+        plt.grid(True)
+        return plt.gca()
+
+    def __call__(self, t):
+        """ Return the voltage value interpolated at time t"""
+        return self.spline(t)
+
 
 if __name__ == '__main__':
     """ Here we test the functionalities of our class. These are not proper
@@ -35,8 +88,8 @@ if __name__ == '__main__':
     # Test printing
     print(v_data)
     # Test plotting
-    plt.figure('voltage vs time')
-    v_data.plot(plt.gca(), fmt='r+')
+    v_data.plot(fmt='ko', markersize=5, label='normal voltage')
+    x_grid = numpy.linspace(min(t), max(t), 200)
+    plt.plot(x_grid, v_data(x_grid), 'r-', label='spline')
+    plt.legend()
     plt.show()
-    
-    
