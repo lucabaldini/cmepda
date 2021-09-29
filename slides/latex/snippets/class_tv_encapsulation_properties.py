734989a603b53c77e1381fb8ac287cf0ce6042af
alberto_manfreda <albertom@pi.infn.it>
2021-09-29 20:24:14 +0200

    Updated snippet

--- slides/latex/snippets/class_tv_encapsulation_properties.py
+++ slides/latex/snippets/class_tv_encapsulation_properties.py
@@ -1,12 +1,9 @@
-""" This time with the special @ syntax, which is much more concise and elegant. """
-
 class Television:
     """ Class describing a televsion.
     """
     def __init__(self, owner):
        """ Class constructor"""
-       # Old-style encapsulation, using setters and getters. Never do that.
-       self._owner = owner
+       self._owner = owner # owner is private
     
     @property
     def owner(self):
@@ -14,11 +11,10 @@ class Television:
 
     @owner.setter
     def owner(self, new_owner):
-       """ This function must be called 'owner'"""
+       """ Make the attribute read-only by acting on the setter"""
        print('Nope {}. Do you want to steal my tv?'.format(new_owner))
  
 tv = Television('Batman')
-# Again: this is the same code as before
 print('This tv belongs to {}'.format(tv.owner)) 
 tv.owner = 'Joker'
 print('This tv belongs to {}'.format(tv.owner))
